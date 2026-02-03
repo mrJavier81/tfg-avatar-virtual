543359771bc2ece819953136ad77ce310b05c201
jsv39ua <jsv39@alu.ua.es>
2026-02-03 16:03:44 +0100

    Añadidas funciones para dibujar los landmarks
    La logica de dibujar los landmarks estan copiados del ejemplo, es mas que nada para visualizar los datos

--- services/vision.js
+++ services/vision.js
@@ -25,12 +25,54 @@ export async function createFaceLandmarker() {
 
 }
 
-createFaceLandmarker();
-
 export function getFaceLandmarker() {
     return faceLandmarker;
 }
 
-export function drawFaceLandmarks(canvas, image, faceLandmarkerResult) {
-    
-}
+export function drawFaceLandmarks(canvasContext, faceLandmarkerResult) {
+
+    const drawingUtils = new DrawingUtils(canvasContext);
+    for (const landmarks of faceLandmarkerResult.faceLandmarks) {
+        drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+            { color: "#C0C0C070", lineWidth: 1 });
+        
+        /*    drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+        { color: "#FF3030" }
+        );
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+        { color: "#FF3030" }
+        );
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+        { color: "#30FF30" }
+        );
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+        { color: "#30FF30" }
+        );
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+        { color: "#E0E0E0" }
+        );
+        drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, {
+        color: "#E0E0E0"
+        });
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+        { color: "#FF3030" }
+        );
+        drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+        { color: "#30FF30" }
+        );*/
+    }
+}   
